0ecabfba7dfdd71ace90502fbec4ab658d27c665
Kelsey Z <you@example.comzahandke@gmail.com>
2017-09-19 12:37:12 -0400

    group project

---
+++ src/CategorySelect.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+import {Link} from "react-router-dom";
+
+import GalleryItem from './GalleryItem';
+
+class CategorySelect extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      data: [],
+    }
+  }
+
+  componentDidMount () {
+
+    fetch('http://circuslabs.net/~michele.james/php/handle_fetch.php?type=categorySelect&query='+this.props.match.params.category,{
+
+        method: 'GET',
+        headers: {
+        },
+    })
+    .then( response => response.json())
+    .then(json => (
+    
+       this.setState({
+        data:json,
+       })
+      
+    ));
+
+    console.log(this.state.data);
+  } 
+
+
+  render() {
+
+    return (
+
+        <ul className="Gallery">
+           {this.state.data.map(item =>
+                <Link to={"/ImageDetail/" + item.id} ><GalleryItem  key={item.id} {...item} className="Container--Gallery__Item"/></Link>)}
+        </ul>
+
+
+    );
+  }
+}
+
+export default CategorySelect;
